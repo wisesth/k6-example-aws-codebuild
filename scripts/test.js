098728158e24e1d994cc1da0f6d1e20c455b099b
wisesth <wisesth@lguplus.co.kr>
2022-10-27 10:58:57 +0900

    add stages

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,14 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const opstions ={
+stages: [
+    { duration: '10s', target: 1000 },
+    { duration: '30s', target: 1000 },
+    { duration: '10s', target: 0 },
+    ],
+}
+
 export default function () {
     const res = http.get('https://test.k6.io');
-    sleep(1);
 }
